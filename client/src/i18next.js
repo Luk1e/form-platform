30b02206bd000ac9014f32b25ed9855f9b04d4f1
Luk1e <luka.gogiashvili.02@gmail.com>
2024-12-17 21:55:15 +0400

    feat: add i18next on frontend

---
+++ client/src/i18next.js
@@ -0,0 +1,31 @@
+import i18n from "i18next";
+import Backend from "i18next-http-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
+import { initReactI18next } from "react-i18next";
+
+i18n
+  .use(Backend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init(
+    {
+      backend: {
+        loadPath: `/static/locales/{{ns}}/{{lng}}.json`,
+      },
+      fallbackLng: "geo",
+      load: "languageOnly",
+      supportedLngs: ["eng", "geo", "rus"],
+      debug: false,
+      ns: ["app", "auth", "admin", "components"],
+      interpolation: {
+        escapeValue: false,
+        formatSeparator: ",",
+      },
+    },
+    (err, t) => {
+      if (err) return console.log("something went wrong loading", err);
+      t("key");
+    }
+  );
+
+export default i18n;
