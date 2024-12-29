61bbff98271fb3fb76e8f3ba7cd0619e049f0700
Luk1e <luka.gogiashvili.02@gmail.com>
2024-12-29 22:46:32 +0400

    fix: reset custom styles on header input

--- client/src/components/header/components/SearchInput.jsx
+++ client/src/components/header/components/SearchInput.jsx
@@ -4,8 +4,8 @@ import { SearchOutlined } from "@ant-design/icons";
 const SearchInput = ({ t }) => (
   <Input
     placeholder={t("global.searchForms")}
-    prefix={<SearchOutlined  />}
-    className="w-full rounded-full border-none outline-none  shadow-none ring-0  transition-all duration-300"
+    prefix={<SearchOutlined />}
+    className="w-full rounded-full"
   />
 );
 
