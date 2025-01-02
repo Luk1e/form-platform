4f8c4f3f47089bc8c784607446790fcbc8bf74ee
Luk1e <luka.gogiashvili.02@gmail.com>
2025-01-02 13:58:54 +0400

    fix: vercel setup N6

--- server/api/app.js
+++ server/api/app.js
@@ -1,8 +1,8 @@
 import express from "express";
-import setupMiddleware from "./config/middleware.js";
-import setupRoutes from "./src/routes/index.js";
-import initializeDatabase from "./config/initializeDatabase.js";
-import setupAdmin from "./config/admin.js";
+import setupMiddleware from "../config/middleware.js";
+import setupRoutes from "../src/routes/index.js";
+import initializeDatabase from "../config/initializeDatabase.js";
+import setupAdmin from "../config/admin.js";
 
 const app = express();
 const PORT = 5000;
