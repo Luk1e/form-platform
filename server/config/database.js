c3210c1890c7c42cdd518f421492847d46c508fa
Luk1e <luka.gogiashvili.02@gmail.com>
2025-01-02 13:29:51 +0400

    deploy: add vercel.json

--- server/config/database.js
+++ server/config/database.js
@@ -10,7 +10,7 @@ const sequelize = new Sequelize(
     host: process.env.DB_HOST,
     dialect: "mysql",
     pool: {
-      max: 10,
+      max: 5,
       min: 0,
       acquire: 30000,
       idle: 10000,
