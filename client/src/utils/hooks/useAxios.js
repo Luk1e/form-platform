4f86af18fd1a401a35d4568fd55f7f208f41313b
Luk1e <luka.gogiashvili.02@gmail.com>
2025-01-02 13:54:49 +0400

    fix: vercel setup N5

--- client/src/utils/hooks/useAxios.js
+++ client/src/utils/hooks/useAxios.js
@@ -6,7 +6,7 @@ export const injectStore = (_store) => {
   store = _store;
 };
 
-const BACKEND_URL = "https://form-platform-lilac.vercel.app";
+const BACKEND_URL = "https://form-platform-lilac.vercel.app/api";
 // const BACKEND_URL = "http://localhost:5000";
 
 export const useAxios = axios.create({
